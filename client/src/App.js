ca6b44323fcca65ff705c5b2db90eaae9206ad6a
ferwin <ferwin.arias@gmail.com>
2022-01-23 17:29:34 -0500

    se configura tailwind para los estilos

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import WhitePaper from "./components/whitePaper/WhitePaper";
-import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
       <Routes>
         <Route path="/" element={<Navbar />}>
-          <Route path="home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="whitePaper" element={<WhitePaper />} />
         </Route>
       </Routes>
-    </div>
   );
 }
 
